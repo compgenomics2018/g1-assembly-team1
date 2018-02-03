48e8e9d0c26b96e8296e27ba8b96b91c6ab252d7
Siarhei Hladyshau <shladyshau3@gatech.edu>
2018-02-03 10:37:30 -0500

    Updated script for running trimmomatic

--- run_trimmomatic.sh
+++ run_trimmomatic.sh
@@ -2,9 +2,8 @@
 
 echo "Run trimmomatic for all files"
 
-for f in $(ls /projects/data/team1_genomeAssembly/downloads/fastq)
+for f in $(cat /projects/home/shladyshau3/g1-assembly-team1/IDs.txt)
 do
-    bn=$(basename ${f} | cut -d . -f 1)
-    echo $bn
-    java -jar /projects/home/shladyshau3/Trimmomatic-0.36/trimmomatic-0.36.jar PE /projects/data/team1_genomeAssembly/downloads/split_fastq/${bn}_1.fastq /projects/data/team1_genomeAssembly/downloads/split_fastq/${bn}_2.fastq /projects/data/team1_genomeAssembly/trimming/${bn}_forward_paired.fq /projects/data/team1_genomeAssembly/trimming/${bn}_forward_unpaired.fq /projects/data/team1_genomeAssembly/trimming/${bn}_reverse_paired.fq /projects/data/team1_genomeAssembly/trimming/${bn}_reverse_unpaired.fq ILLUMINACLIP:/projects/home/shladyshau3/Trimmomatic-0.36/adapters/NexteraPE-PE.fa:2:30:10 LEADING:5 TRAILING:5 SLIDINGWINDOW:4:20 MINLEN:20 CROP:245 HEADCROP:20 
+    echo $f
+    java -jar /projects/home/shladyshau3/Trimmomatic-0.36/trimmomatic-0.36.jar PE /projects/data/team1_genomeAssembly/downloads/split_fastq/${f}_1.fastq /projects/data/team1_genomeAssembly/downloads/split_fastq/${f}_2.fastq /projects/data/team1_genomeAssembly/trimming/${f}_forward_paired.fq /projects/data/team1_genomeAssembly/trimming/${f}_forward_unpaired.fq /projects/data/team1_genomeAssembly/trimming/${f}_reverse_paired.fq /projects/data/team1_genomeAssembly/trimming/${f}_reverse_unpaired.fq ILLUMINACLIP:/projects/home/shladyshau3/Trimmomatic-0.36/adapters/NexteraPE-PE.fa:2:30:10 LEADING:5 TRAILING:5 CROP:245 HEADCROP:22 SLIDINGWINDOW:4:20 MINLEN:20 
 done
