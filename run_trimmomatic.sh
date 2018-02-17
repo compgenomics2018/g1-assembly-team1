a5c99220ba4a867cf1515274ef582664b8738133
Shah, Nirav Nimesh <niravs@gatech.edu>
2018-02-17 16:21:53 -0500

    Update run_trimmomatic.sh

--- run_trimmomatic.sh
+++ run_trimmomatic.sh
@@ -11,7 +11,7 @@ mkdir $out_fold/fastq
 for f in $(cat /projects/home/shladyshau3/g1-assembly-team1/IDs.txt)
 do
     echo $f
-    java -jar /projects/home/shladyshau3/Trimmomatic-0.36/trimmomatic-0.36.jar PE /projects/data/team1_genomeAssembly/downloads/split_fastq/${f}_1.fastq /projects/data/team1_genomeAssembly/downloads/split_fastq/${f}_2.fastq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_forward_paired.fq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_forward_unpaired.fq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_reverse_paired.fq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_reverse_unpaired.fq ILLUMINACLIP:/projects/home/shladyshau3/map_reads_to_adapters/adapters.fa:2:30:10 LEADING:5 TRAILING:5 CROP:245 HEADCROP:15 SLIDINGWINDOW:4:20 MINLEN:20 >> $out_fold/trimmomatic.log
+    java -jar /projects/home/shladyshau3/Trimmomatic-0.36/trimmomatic-0.36.jar PE /projects/data/team1_genomeAssembly/downloads/split_fastq/${f}_1.fastq /projects/data/team1_genomeAssembly/downloads/split_fastq/${f}_2.fastq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_forward_paired.fq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_forward_unpaired.fq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_reverse_paired.fq /projects/data/team1_genomeAssembly/trimming/fastq/${f}_reverse_unpaired.fq ILLUMINACLIP:/projects/home/shladyshau3/map_reads_to_adapters/adapters.fa:5:30:10 SLIDINGWINDOW:4:20 MINLEN:20 >> $out_fold/trimmomatic.log
 done
 
 
