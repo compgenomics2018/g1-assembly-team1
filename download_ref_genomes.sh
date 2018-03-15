011591e40a80830ef23023d53b83ce575dda4927
Siarhei Hladyshau <shladyshau3@gatech.edu>
2018-03-15 18:58:59 -0400

    corrected path for upload

--- download_ref_genomes.sh
+++ download_ref_genomes.sh
@@ -5,8 +5,7 @@ wget ftp://ftp.ncbi.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt
 awk -F "\t" '/Klebsiella/ && /Complete/ && !/phage/ { split($20,a,"/"); print $20 "/" a[10] "_genomic.fna.gz" }' assembly_summary_refseq.txt > download_list.txt
 
 mkdir genomes_download
-cd genomes_download
-wget -i download_list.txt -P genomes_download
+wget -i download_list.txt -P ./genomes_download
 
 
 for f in `ls genomes_download`;
